013580d9d2fe4b27d87cbb456dfaa1a9e3d38b26
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2022-07-09 02:05:31 -0500

    Electron Notification Work
    Added some weather alert notification functionality

--- electron/index.js
+++ electron/index.js
@@ -1,5 +1,11 @@
-const { app, BrowserWindow, Notification, Tray, Menu} = require('electron')
+const { app, BrowserWindow, Notification, Tray, Menu, net} = require('electron')
 var win2 = null;
+var weatherLocations;
+var locationNames;
+var locationCache;
+var settings;
+var notifiedAlerts = [];
+var cycleAt = 0;
 const createWindow = () => {
 const win = new BrowserWindow({
     width: 800,
@@ -20,14 +26,18 @@ else{
 		openAtLogin: true
 	})
 	app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
-		if (win2) {
-			if (win2.isMinimized()) win2.restore()
-				win2.focus()
-			}
+		if (win2 == null){
+			createWindow();
+		}
+		win2.show()
+		if (win2.isMinimized()){
+			win2.restore();
+			win2.focus();
+		}
 	})
 	app.whenReady().then(() => {
 		if (process.platform === 'win32'){
-			app.setAppUserModelId(app.name);
+			app.setAppUserModelId("Atmos Weather");
 		}
 		createWindow()
 		let trayIcon = new Tray(__dirname + "/img/icon.png")
@@ -85,10 +95,10 @@ setInterval(function(){
 	if (win2 == null){
 		createWindow();
 	}
-	var weatherLocations;
-	var locationNames;
-	var locationCache;
-	var settings;
+//	win2.webContents.executeJavaScript('localStorage.getItem("notified-alerts");', true)
+//		.then(result => {
+//			notifiedAlerts = JSON.parse(result);
+//	  	});
 	win2.webContents.executeJavaScript('localStorage.getItem("weather-locations");', true)
 		.then(result => {
 			weatherLocations = JSON.parse(result);
@@ -104,5 +114,87 @@ setInterval(function(){
 	win2.webContents.executeJavaScript('localStorage.getItem("atmos-settings")', true)
 		.then(result => {
 		settings = JSON.parse(result);
-	});	
-}, 5000);
+	});
+	setTimeout(checkLocation, 100);
+	
+	cycleAt++;
+}, 10000);
+
+// Check the location for alerts
+function checkLocation(){
+	if (locationNames == undefined){
+		setTimeout(checkLocation, 200);
+		return;
+	}
+	if (cycleAt >= locationNames.length){
+		cycleAt = 0;
+	}
+	if (locationNames.length > 0){
+		var locationAlerts = alertCheck("https://api.weather.gov/alerts/active?point=" + weatherLocations[cycleAt]["lat"] + "," + weatherLocations[cycleAt]["lon"]);
+	}
+}
+
+// HTTP Get Call
+function alertCheck(urlGet){
+	try{
+		var request = net.request(urlGet);
+		request.on("response", (response) => {
+			response.on('data', (chunk) => {
+				if (!notifiedAlerts){
+					notifiedAlerts = [];
+				}
+				chunk = JSON.parse(chunk);
+				chunk = chunk["features"];
+				var at = 0;
+				while (at < chunk.length){
+					if (!notifiedAlerts.includes(chunk[at]["id"])){
+						console.log(locationNames[cycleAt])
+						var eventType = chunk[at]["properties"]["event"];
+						if (eventType.includes("Red Flag Warning")){
+							eventType = "Fire Weather Warning";
+						}
+						var notificationSetting = "soundnotification";
+						eventType = eventType.toLowerCase().replaceAll(" ", "-");
+						if (eventType.includes("warning")){
+							notificationSetting = settings["alert-types"]["warnings"][eventType.replace("-warning", "")];
+						}
+						else if (eventType.includes("watches")){
+							notificationSetting = settings["alert-types"]["watches"][eventType.replace("-watch", "")];
+						}
+						else{
+							notificationSetting = settings["alert-types"]["advisory"][eventType.replace("-advisory", "")];
+						}
+						if (settings["per-location"][locationNames[cycleAt]] != undefined){
+							if (eventType.includes("warning")){
+								if (settings["per-location"][locationNames[cycleAt]]["alert-types"]["warnings"][eventType.replace("-warning", "")] != undefined){
+									notificationSetting = settings["per-location"][locationNames[cycleAt]]["alert-types"]["warnings"][eventType.replace("-warning", "")];
+								}
+							}
+							else if (eventType.includes("watches")){
+								if (settings["per-location"][locationNames[cycleAt]]["alert-types"]["watches"][eventType.replace("-watch", "")] != undefined){
+									notificationSetting = settings["per-location"][locationNames[cycleAt]]["alert-types"]["watches"][eventType.replace("-watch", "")];
+								}
+								notificationSetting = settings["alert-types"]["watches"][eventType.replace("-watch", "")];
+							}
+							else{
+								if (settings["per-location"][locationNames[cycleAt]]["alert-types"]["advisory"][eventType.replace("-advisory", "")] != undefined){
+									notificationSetting = settings["per-location"][locationNames[cycleAt]]["alert-types"]["advisory"][eventType.replace("-advisory", "")];	
+								}
+							}
+						}
+						new Notification({ title: chunk[at]["properties"]["event"] + " issued for " + locationNames[cycleAt], body: chunk[at]["properties"]["description"], urgency: "critical", timeoutType: 'never', silent: true, sound: __dirname + "/audio/readynownotification.mp3", icon: __dirname + "/img/warning.png"}).show()
+						win2.webContents.executeJavaScript("var audio = new Audio('audio/readynownotification.mp3');audio.play();", false);
+						locationNames[cycleAt]
+						console.log(notificationSetting);
+						notifiedAlerts.push(chunk[at]["id"]);
+					}
+					at++;
+				}
+			})
+		})
+		request.end();
+	}
+	catch(err){
+		console.log("Computer may be falling asleep.")
+	}
+}
