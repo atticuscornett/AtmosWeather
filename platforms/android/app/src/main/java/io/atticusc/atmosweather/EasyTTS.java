caff95d89a1c486143b4ec6651ae89ffee38ddeb
Roark McKibben <roarkmckibben@gmail.com>
2023-01-28 21:16:31 -0600

    Tweaked Comments in EasyTTS.java

--- platforms/android/app/src/main/java/io/atticusc/atmosweather/EasyTTS.java
+++ platforms/android/app/src/main/java/io/atticusc/atmosweather/EasyTTS.java
@@ -9,21 +9,15 @@ public class EasyTTS {
     TextToSpeech tts;
     public EasyTTS(String text, Context ctx){
         try {
-            tts = new TextToSpeech(ctx, new TextToSpeech.OnInitListener() {
-                @Override
-                public void onInit(int i) {
-
-                    // if No error is found then only it will run
-                    if (i != TextToSpeech.ERROR) {
-                        // To Choose language of speech
-                        tts.setLanguage(Locale.US);
-                        tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
-                    }
+            tts = new TextToSpeech(ctx, i -> {
+                // if no error is found then only it will run
+                if (i != TextToSpeech.ERROR) {
+                    // to choose language of speech
+                    tts.setLanguage(Locale.US);
+                    tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
                 }
-
             });
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("TTS fail");
         }
     }
