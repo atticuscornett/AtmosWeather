cf85bc43bf8c8fc5b1bffcc93da99f63cf632532
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-11-27 00:11:15 -0600

    Widget Update
    Updated widget layout (hopefully for the final time). Added temperature for current location.

--- android/app/src/main/java/io/atticusc/atmosweather/WeatherBar.java
+++ android/app/src/main/java/io/atticusc/atmosweather/WeatherBar.java
@@ -3,9 +3,21 @@ package io.atticusc.atmosweather;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,22 +25,61 @@ import java.text.SimpleDateFormat;
 public class WeatherBar extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int appWidgetId) throws JSONException {
 
-        CharSequence widgetText = "Updated " + new SimpleDateFormat("HH:mm").format(new java.util.Date());
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_bar);
-        views.setTextViewText(R.id.lastUpdatedText, widgetText);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("NativeStorage", Context.MODE_MULTI_PROCESS);
+        JSONObject currentLocationProps = new JSONObject(sharedPreferences.getString("currentLocationProperties", "{}"));
+        if (currentLocationProps.length() != 0){
+            System.out.println(currentLocationProps);
+            currentLocationProps.getString("forecast");
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(context);
+            String url = currentLocationProps.getString("forecast") + "/hourly";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject forecastResponse = new JSONObject(response);
+                                int temp = forecastResponse.getJSONObject("properties").getJSONArray("periods").getJSONObject(0).getInt("temperature");
+                                CharSequence widgetText = new SimpleDateFormat("HH:mm").format(new java.util.Date());
+                                CharSequence tempText = temp + " °F";
+                                // Construct the RemoteViews object
+                                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_bar);
+                                views.setTextViewText(R.id.lastUpdatedText, widgetText);
+                                views.setTextViewText(R.id.currentTempWidget, tempText);
+
+                                // Instruct the widget manager to update the widget
+                                appWidgetManager.updateAppWidget(appWidgetId, views);
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }, null) {
+                @Override
+                public Map<String, String> getHeaders(){
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("User-agent", "Atmos Weather Widget Service");
+                    return headers;
+                }
+            };
+            queue.add(stringRequest);
+        }
+        System.out.println("hey good news this works");
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            try {
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
