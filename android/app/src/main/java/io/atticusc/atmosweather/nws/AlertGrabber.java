938bc028a3a86071e215624fff0e3f2d9d7976b4
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-03-25 18:53:52 -0500

    Create Area Alert Links Android

---
+++ android/app/src/main/java/io/atticusc/atmosweather/nws/AlertGrabber.java
@@ -0,0 +1,59 @@
+package io.atticusc.atmosweather.nws;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class AlertGrabber {
+    private static ArrayList<String> getAllUGCCodes(JSONObject locationCache, JSONArray locationNames) throws JSONException {
+        ArrayList<String> codes = new ArrayList<>();
+        String locationName;
+        for (int i = 0; i < locationNames.length(); i++){
+            locationName = locationNames.getString(i);
+            try {
+                JSONObject locationData = new JSONObject(locationCache.getString(locationName));
+                JSONObject properties = locationData.getJSONObject("properties");
+
+                if (properties.has("county")){
+                    String county = properties.getString("county");
+                    county = county.substring(county.length() - 6);
+                    if (!codes.contains(county)){
+                        codes.add(county);
+                    }
+                }
+
+                if (properties.has("forecastZone")){
+                    String forecastZone = properties.getString("forecastZone");
+                    forecastZone = forecastZone.substring(forecastZone.length() - 6);
+                    if (!codes.contains(forecastZone)){
+                        codes.add(forecastZone);
+                    }
+                }
+
+                if (properties.has("fireWeatherZone")){
+                    String fireWeatherZone = properties.getString("fireWeatherZone");
+                    fireWeatherZone = fireWeatherZone.substring(fireWeatherZone.length() - 6);
+                    if (!codes.contains(fireWeatherZone)){
+                        codes.add(fireWeatherZone);
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return codes;
+    }
+
+    public static String createAlertLink(JSONObject locationCache, JSONArray locationNames){
+        ArrayList<String> ugcCodes = new ArrayList<>();
+        try {
+            ugcCodes = getAllUGCCodes(locationCache, locationNames);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return "https://api.weather.gov/alerts/active?zone=" + String.join(",", ugcCodes);
+    }
+}
