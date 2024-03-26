bf74b1a4321aa7ba8a8082c3d523f706c106d65d
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-03-25 19:46:14 -0500

    Determine location for alert object

--- android/app/src/main/java/io/atticusc/atmosweather/nws/AlertGrabber.java
+++ android/app/src/main/java/io/atticusc/atmosweather/nws/AlertGrabber.java
@@ -4,8 +4,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Set;
+
+import org.geotools.geojson.geom.GeometryJSON;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
 
 public class AlertGrabber {
     private static ArrayList<String> getAllUGCCodes(JSONObject locationCache, JSONArray locationNames) throws JSONException {
@@ -55,5 +62,60 @@ public class AlertGrabber {
             throw new RuntimeException(e);
         }
         return "https://api.weather.gov/alerts/active?zone=" + String.join(",", ugcCodes);
+    }
+
+    private static String determineAlertLocation(JSONObject alert, JSONArray locationNames, JSONArray weatherLocations, JSONObject locationCache) {
+        if (alert.has("geometry")){
+            try {
+                GeometryJSON geoJsonReader = new GeometryJSON();
+                JSONObject alertGeometryJSON = alert.getJSONObject("geometry");
+                Polygon alertGeometry = (Polygon) geoJsonReader.read(alertGeometryJSON.toString());
+                GeometryFactory geometryFactory = new GeometryFactory();
+                for (int i = 0; i < weatherLocations.length(); i++) {
+                    JSONObject location = weatherLocations.getJSONObject(i);
+                    Point locationPoint = geometryFactory.createPoint(
+                            new Coordinate(location.getDouble("longitude"), location.getDouble("latitude"))
+                    );
+                    if (alertGeometry.contains(locationPoint)){
+                        return locationNames.getString(i);
+                    }
+                }
+            } catch (JSONException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            try {
+                JSONArray zones = alert.getJSONObject("properties").getJSONArray("affectedZones");
+                for (int i = 0; i < zones.length(); i++) {
+                    String zone = zones.getString(i);
+                    for (int j = 0; j < locationNames.length(); j++) {
+                        JSONObject location = locationCache.getJSONObject(locationNames.getString(j));
+                        String countyUGC = "";
+                        String forecastZoneUGC = "";
+                        String fireWeatherZoneUGC = "";
+
+                        if (location.getJSONObject("properties").has("county")){
+                            countyUGC = location.getJSONObject("properties").getString("county");
+                        }
+
+                        if (location.getJSONObject("properties").has("forecastZone")){
+                            forecastZoneUGC = location.getJSONObject("properties").getString("forecastZone");
+                        }
+
+                        if (location.getJSONObject("properties").has("fireWeatherZone")){
+                            fireWeatherZoneUGC = location.getJSONObject("properties").getString("fireWeatherZone");
+                        }
+
+                        if (zone.equals(countyUGC) || zone.equals(forecastZoneUGC) || zone.equals(fireWeatherZoneUGC)){
+                            return locationNames.getString(j);
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
     }
 }
