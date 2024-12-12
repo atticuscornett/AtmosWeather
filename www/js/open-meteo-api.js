7a3e0ee3980d406fca2b8097e65875f624f1850c
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-12-12 00:46:23 -0600

    Clean up open-meteo-api.js

--- www/js/open-meteo-api.js
+++ www/js/open-meteo-api.js
@@ -1,5 +1,12 @@
+/**
+ * Fetches the current Air Quality Index (AQI) for a given latitude and longitude.
+ *
+ * @param {number} lat - The latitude of the location.
+ * @param {number} long - The longitude of the location.
+ * @param {function} callback - The callback function to handle the AQI data.
+ */
 function getCurrentAQIForPositionAsync(lat, long, callback){
-    window.loadingElements++;
+    addLoadingKey("currentAQI" + lat + long);
     fetch("https://air-quality-api.open-meteo.com/v1/air-quality?latitude=" + lat + "&longitude=" + long + "&hourly=us_aqi")
         .then(data => data.json())
         .then(data => {
@@ -8,13 +15,21 @@ function getCurrentAQIForPositionAsync(lat, long, callback){
             time = time.split(":");
             time = time[0] + ":00";
             let index = data["hourly"]["time"].indexOf(time);
-            window.loadingElements--;
+            removeLoadingKey("currentAQI" + lat + long);
             callback(data["hourly"]["us_aqi"][index]);
         })
 }
 
+/**
+ * Fetches the current Air Quality Index (AQI) for a given nominatim object.
+ *
+ * @param {Object} nom - The nominatim object containing latitude and longitude.
+ * @param {number} nom.lat - The latitude of the location.
+ * @param {number} nom.lon - The longitude of the location.
+ * @param {function} callback - The callback function to handle the AQI data.
+ */
 function getCurrentAQIForNomAsync(nom, callback){
-    window.loadingElements++;
+    addLoadingKey("currentAQI" + nom["lat"] + nom["lon"]);
     fetch("https://air-quality-api.open-meteo.com/v1/air-quality?latitude=" + nom["lat"] + "&longitude=" + nom["lon"] + "&hourly=us_aqi")
         .then(data => data.json())
         .then(data => {
@@ -23,13 +38,20 @@ function getCurrentAQIForNomAsync(nom, callback){
             time = time.split(":");
             time = time[0] + ":00";
             let index = data["hourly"]["time"].indexOf(time);
-            window.loadingElements--;
+            removeLoadingKey("currentAQI" + nom["lat"] + nom["lon"]);
             callback(data["hourly"]["us_aqi"][index]);
         })
 }
 
+/**
+ * Fetches additional weather data for a given latitude and longitude.
+ *
+ * @param {number} lat - The latitude of the location.
+ * @param {number} long - The longitude of the location.
+ * @param {function} callback - The callback function to handle the weather data.
+ */
 function getAdditionalWeatherDataForPositionAsync(lat, long, callback){
-    window.loadingElements++;
+    addLoadingKey("additionalWeather" + lat + long);
     fetch("https://api.open-meteo.com/v1/forecast?latitude=" + lat
         + "&longitude=" + long
         /* Current parameters */ + "&current=apparent_temperature"
@@ -37,13 +59,21 @@ function getAdditionalWeatherDataForPositionAsync(lat, long, callback){
         /* Unit settings */ + "&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch")
         .then(data => data.json())
         .then(data => {
-            window.loadingElements--;
+            removeLoadingKey("additionalWeather" + lat + long);
             callback(data);
         })
 }
 
+/**
+ * Fetches additional weather data for a given nominatim object.
+ *
+ * @param {Object} nom - The nominatim object containing latitude and longitude.
+ * @param {number} nom.lat - The latitude of the location.
+ * @param {number} nom.lon - The longitude of the location.
+ * @param {function} callback - The callback function to handle the weather data.
+ */
 function getAdditionalWeatherDataForNomAsync(nom, callback){
-    window.loadingElements++;
+    addLoadingKey("additionalWeather" + nom["lat"] + nom["lon"]);
     fetch("https://api.open-meteo.com/v1/forecast?latitude=" + nom["lat"]
         + "&longitude=" + nom["lon"]
         /* Current parameters */ + "&current=apparent_temperature"
@@ -51,11 +81,18 @@ function getAdditionalWeatherDataForNomAsync(nom, callback){
         /* Unit settings */ + "&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch")
         .then(data => data.json())
         .then(data => {
-            window.loadingElements--;
+            removeLoadingKey("additionalWeather" + nom["lat"] + nom["lon"]);
             callback(data);
         })
 }
 
+/**
+ * Removes old data points from the provided times and data arrays, keeping only the most recent data.
+ *
+ * @param {Array} times - An array of time strings in ISO format.
+ * @param {Array} data - An array of data points corresponding to the times.
+ * @returns {Array} - A tuple containing the filtered times and data arrays.
+ */
 function removeOldData(times, data){
     let time = new Date();
     let UTCHour = time.getUTCHours();
