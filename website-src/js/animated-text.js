6dc5e3d8f5c2c02af8ae9190767363cd0275c6ea
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2025-06-22 22:24:03 -0500

    Add animated text

---
+++ website-src/js/animated-text.js
@@ -0,0 +1,35 @@
+let people = ["privacy enthusiasts", "the detail-oriented", "outdoor adventurers", "the health-conscious",
+    "open-source lovers", "customization fans", "desktop + mobile", "travelers", "the weather-aware", "everyone"];
+let index = Math.floor(Math.random() * people.length);
+
+let typeTextIn = () => {
+    let text = people[index];
+    document.getElementById("animated-text").innerText = text[0];
+
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            document.getElementById("animated-text").innerText = text.slice(0, i + 1);
+        }, 50 * i);
+    }
+
+    setTimeout(typeTextOut, (50*text.length) + 20*1000);
+}
+
+let typeTextOut = () => {
+    let text  = document.getElementById("animated-text").innerText;
+    for (let i = text.length - 1; i >= 1; i--) {
+        setTimeout(() => {
+            document.getElementById("animated-text").innerText = text.slice(0, i);
+        }, 50 * (text.length - i));
+    }
+
+    index++;
+    if (index >= people.length) {
+        index = 0;
+    }
+
+    setTimeout(typeTextIn, (50*text.length) + 100);
+}
+
+typeTextIn();
