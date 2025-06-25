4e663edad550e623b5da106b7cba735c8ba8e5ed
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2025-06-24 23:41:36 -0500

    v3.0.0
    Update all update notes, version codes, and files to v3.0.0

--- electron/js/start.js
+++ electron/js/start.js
@@ -5,19 +5,24 @@ setTimeout(() => {
 
 let updateFiles = false;
 
-window.atmosVersion = "3.0.0-beta.1";
-window.atmosUpdated = "6-22-2025";
-window.atmosUpdateTitle = "Atmos Weather v3.0.0 Beta One is here!";
+window.atmosVersion = "3.0.0";
+window.atmosUpdated = "6-24-2025";
+window.atmosUpdateTitle = "Atmos Weather v3.0.0 is here!";
 window.atmosUpdateNotes = `
 		<dl style='font-family: Secular One, sans-serif;'>
-		    <dt>This is a beta release!</dt>
-		    <dd>- Release notes will be made available for the full release.</dd>
-		    <dd>- Please report any bugs you find to the Atmos Weather GitHub repo.</dd>
-<!--			<dt>Bug Fixes</dt>-->
-<!--			<dd>- Fixed wind speed graph bug.</dd>-->
-<!--			<dd>- Fixed battery optimization permission UI bug.</dd>-->
-<!--			<dd>- Fixed permission window repeatedly showing on first installation.</dd>-->
-<!--			<dd>- Updated packages.</dd>-->
+		    <dt>New Features</dt>
+		    <dd>- Added widget system for customizable location pages</dd>
+		    <dd>- Added 5 widget templates</dd>
+		    <dd>- Added 9 new widgets</dd>
+		    <dd>- Added full support for 39 new alert types</dd>
+		    <dd>- Alerts now sorted by priority in settings</dd>
+		    <dd>- Jump to radar from location page</dd>
+		    <dd>- Back button now supported on Android</dd>
+		    <dd>- Quiet hours now available on Android</dd>
+		    <dt>Bug Fixes</dt>
+		    <dd>- Fixed wind speed graph icon bug</dd>
+		    <dd>- Android app launch performance improved</dd>
+		    <dd>- Updated dependencies</dd>
 		</dl> 
 		`;
 
