d2bbf069020c8a5e28db8dfbbd67aa86356d83a6
Atticus Cornett <64325242+atticuscornett@users.noreply.github.com>
2026-05-18 00:35:22 -0500

    Reorganize Resources

--- src/Pages/AboutPage.svelte
+++ src/Pages/AboutPage.svelte
@@ -10,21 +10,22 @@
     <h3 id="atmos-app-updated">Updated on {window.atmosUpdated}</h3>
     <h6>Written by Atticus Cornett and <a href="https://github.com/atticuscornett/AtmosWeather/graphs/contributors" target="_blank">contributors</a></h6>
     <hr>
+    <br>
     <details>
         <summary>Update Notes</summary>
         <h4>{@html window.atmosUpdateNotes}</h4>
     </details>
     <br>
     <h2>Resources</h2>
-    <h3><a href="https://tablericons.com/" target="_blank">Tabler Icons</a></h3>
-    <h3><a href="https://capacitorjs.com/" target="_blank">CapacitorJS</a></h3>
-    <h3><a href="https://svelte.dev/" target="_blank">Svelte</a></h3>
-    <h3><a href="https://github.com/TheCocoaProject/cordova-plugin-nativestorage" target="_blank">Cordova NativeStorage Plugin</a></h3>
-    <h3><a href="https://fonts.google.com/" target="_blank">Google Fonts</a></h3>
-    <h3><a href="https://nominatim.org/" target="_blank">OSM Nominatim</a></h3>
-    <h3><a href="https://www.weather.gov/documentation/services-web-api" target="_blank">NWS API</a></h3>
-    <h3><a href="https://open-meteo.com/" target="_blank">Open-Meteo API</a></h3>
-    <h3><a href="https://leafletjs.com/index.html" target="_blank">LeafletJS</a></h3>
-    <h3><a href="https://www.electronjs.org/" target="_blank">ElectronJS</a></h3>
-    <h3><a href="https://storyset.com/people" target="_blank">People illustrations by Storyset</a></h3>
+    <h3><a href="https://capacitorjs.com/" target="_blank">CapacitorJS (Android App)</a></h3>
+    <h3><a href="https://github.com/TheCocoaProject/cordova-plugin-nativestorage" target="_blank">Cordova NativeStorage Plugin (Android App)</a></h3>
+    <h3><a href="https://www.electronjs.org/" target="_blank">ElectronJS (Desktop Apps)</a></h3>
+    <h3><a href="https://fonts.google.com/" target="_blank">Google Fonts (Fonts)</a></h3>
+    <h3><a href="https://leafletjs.com/index.html" target="_blank">LeafletJS (Maps)</a></h3>
+    <h3><a href="https://www.weather.gov/documentation/services-web-api" target="_blank">NWS API (Weather Data)</a></h3>
+    <h3><a href="https://open-meteo.com/" target="_blank">Open-Meteo API (Weather Data)</a></h3>
+    <h3><a href="https://nominatim.org/" target="_blank">OSM Nominatim (Location Search)</a></h3>
+    <h3><a href="https://storyset.com/people" target="_blank">Storyset (Illustrations)</a></h3>
+    <h3><a href="https://svelte.dev/" target="_blank">Svelte (Framework)</a></h3>
+    <h3><a href="https://tablericons.com/" target="_blank">Tabler Icons (Icons)</a></h3>
 </TabSlot>
